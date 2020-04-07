fac91b003f1824d1a573c831426ebc45d16432aa
kane2033 <u-cent@yandex.ru>
2020-04-07 17:25:30 +0700

    Добавлена новая версия крестиков-ноликов без серверной части: размерность игровой доски и количество фигур, необходимых для победы, могут быть изменены (меняется в коде); на данный момент размерность доски = 19х19, кол-во фигур для победы = 5; при окончании игры работает кнопка перезапуска.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+//клетка, которая будет содержать Х или О
 function Square(props) {
     return (
         <button className="square" onClick={props.onClick}>
@@ -20,86 +21,118 @@ function RestartButton(props) {
 }
 
 //класс доски, хранящий массив крестиков и ноликов
+//и состояние игры
 class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(9).fill(null),
+            boardLength: props.boardLength,
+            winCondition: props.winCondition,
             xIsNext: true,
+            squares: this.instantiate2DArray(props.boardLength),
+            winner: null,
+            isDraw: false,
         }
+    }
+
+    //метод инициализации двумерного массива и заполнение Null
+    instantiate2DArray(length) {
+        let array = new Array(length);
+        for (let i = 0; i < length; i++) {
+            array[i] = new Array(length);
+            array[i].fill(null);
+        }
+        return array;
     }
 
     //метод при нажатии на квадрат:
     //ставит Х или О в зависимости от того, кто ходит
-    handleClick(i) {
-        const squares = this.state.squares.slice();
+    //а также меняет состояние игрового поля
+    handleClick(i, j) {
+        let squares = this.state.squares;
         //если есть победитель или поле заполнено, клик игнорируется
-        if (calculateWinner(squares) || squares[i]) {
+        if (this.state.winner || squares[i][j]) {
             return;
         }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
+        squares[i][j] = this.state.xIsNext ? 'X' : 'O'; //вставка соответствующего символа
+        this.setState({ //изменение состояния игры
+            squares: squares, //новая версия массива клеток с только что поставленным символом
+            xIsNext: !this.state.xIsNext, //меняет очередь
+            winner: calculateWinner(squares, i, j, this.state.winCondition), //проверка на победителя
+            isDraw: this.isDraw(squares), //проверка на ничью
         });
     }
 
-    restartGame() {
-        this.setState(this.state.squares.fill(null));
+    //метод проверки на ничью:
+    //если все клетки заняты, то это считается ничьей
+    isDraw(squares) {
+        for (let i = 0; i < squares.length; i++) {
+            for (let j = 0; j < squares.length; j++) {
+                if (squares[i][j] === null) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
-    //метод при создании клетки передает значение
-    //из массива значений Х и О в эту клетку, и
-    //передается метод, обрабатывающий клик
-    renderSquare(i) {
+    //метод перезапуска игры:
+    //возвращает состояние игры в изначальное положение
+    restartGame() {
+        this.setState({
+            squares: this.instantiate2DArray(this.state.boardLength),
+            winner: null,
+            xIsNext: true,
+            isDraw: false,
+        });
+    }
+
+    //метод отрисовки клетки
+    renderSquare(i, j) {
         return (
             <Square
-                value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
+                value={this.state.squares[i][j]}
+                onClick={() => this.handleClick(i, j)}
             />
         );
     }
 
+    //метод отрисовки кнопки перезапуска;
+    //кнопка отрисовывается при нахождении победителя или ничьи
     renderRestartButton(disabled) {
         return disabled ? null : (
           <RestartButton
               onClick={() => this.restartGame()}
-              //disabled={disabled}
           />
         );
     }
 
-    //отрисовывает игровое поле из 9 клеток
+    //возвращает игровое поле с размером, указанном в состоянии
+    renderBoard() {
+        let len = this.state.boardLength;
+        let boardArray = new Array(len);
+        let lineArray = new Array(len);
+        for (let i = 0; i < len; i++) {
+            for (let j = 0; j < len; j++) {
+                lineArray[j] = this.renderSquare(i, j);
+            }
+            boardArray[i] = <div className="board-row">{lineArray.slice()}</div>;
+
+        }
+        return boardArray;
+    }
+
+    //отрисовывает игровое поле каждый клик
     render() {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if (winner) { // если winner != null
-            status = 'Выиграл ' + winner;
-            //make restart button visible!!!
-        }
-        else {
-            status = 'Следующий ход: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
+        let status = this.state.winner ? 'Выиграл ' + this.state.winner :
+            this.state.isDraw ? 'Ничья!' :
+        'Следующий ход: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
                 <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-                {this.renderRestartButton(calculateWinner(this.state.squares) == null)}
+                {this.renderBoard()}
+                {this.renderRestartButton(this.state.winner == null && this.state.isDraw === false)}
             </div>
         );
     }
@@ -110,42 +143,62 @@ class Game extends React.Component {
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board
+                        boardLength={19}//размер поля
+                        winCondition={5}//фигур для победы
+                    />
                 </div>
-                {/*<div className="game-info">*/}
-                {/*    <div>/!* status *!/</div>*/}
-                {/*    <ol>/!* TODO *!/</ol>*/}
-                {/*</div>*/}
             </div>
         );
     }
 }
 
-//метод проверки победителя:
-//возвращает X, O или null
-//(для бесконечного поля следует поменять метод)
-function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
+//проверка победителя
+function calculateWinner(squares, i, j, winCondition) {
+    let winner = null;
+    let directions = [ //массив всех направлений проверки по часовой стрелке
+        [-1, -1],
+        [-1, 0],
+        [-1, 1],
+        [0, 1],
+        [1, 1],
+        [1, 0],
+        [1, -1],
+        [0, -1]
     ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
+    //перебор всех возможных вариантов (их 8):
+    for (let k = 0; winner == null && k < directions.length; k++) {
+        winner = checkWinDirection(squares, i, j, directions[k][0], directions[k][1], winCondition);
     }
-    return null;
+
+    //проверка победителя в заданном направлении
+    //d1 и d2 - это направление проверки
+    //пр.: d1 = 0; d2 = 1 => проверка идет по горизонтали вправо
+    //d1 = -1; d2 = -1; => проверка идет по диагонали влево вверх
+    function checkWinDirection(squares, i, j, d1, d2, winCondition) {
+        let score = 1;
+        let symbol = squares[i][j];
+        let x = i;
+        let y = j;
+        x += d1;
+        y += d2;
+        //пока не вышли из границ поля и проверяемый на победу символ присутствует, считаются очки и проверяется дальше
+        while ((x >= 0 && y >= 0 && x < squares.length && y < squares.length) && (squares[x][y] === symbol)) {
+            score++;
+            x += d1;
+            y += d2;
+        }
+        //если в ряд набралось достаточно победителей, возвращает победителя
+        if (score === winCondition) {
+            return symbol === 'X' ? 'X' : 'O';
+        }
+        else return null; //означает отсутствие победителя
+    }
+
+    return winner;
 }
 
-// ========================================
-
+//рендер всех компонентов
 ReactDOM.render(
     <Game />,
     document.getElementById('root')
