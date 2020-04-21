eebe9639cfc4f17e8eb143dba3fc8b5e703dd2b9
kane2033 <u-cent@yandex.ru>
2020-04-21 22:19:47 +0700

    Typescript добавлен в проект для создания enum.

---
+++ src/enum/gameStatus.ts
@@ -0,0 +1,7 @@
+export enum gameStatus {
+    In_Progress = 'In_Progress',
+    Player1_Won = 'Player1_Won',
+    Player2_Won = 'Player2_Won',
+    Tie = 'Tie',
+    Waiting_Player2 = 'Waiting_Player2',
+}
